f905fb9758a6d681781f61c510b1c9f10e887d9f
BotSeven <75718422+boatbotgithub@users.noreply.github.com>
2021-11-08 18:25:01 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,74 @@
-const fill = document.querySelector('.fill1');
-const empties = document.querySelectorAll('.empty');
-
-// Fill listeners
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
+const weapons = document.querySelectorAll('.weapon');
 
 
-// Loop through empty boxes and add listeners
-for (const empty of empties) {
-  empty.addEventListener('dragover', dragOver);
-  empty.addEventListener('dragenter', dragEnter);
-  empty.addEventListener('dragleave', dragLeave);
-  empty.addEventListener('drop', dragDrop);
+const equipSlot1 = document.querySelector('.equip-slot-1');
+const equipWeapon1 = document.querySelector('.equip-weapon-1');
+
+
+const equipSlot2 = document.querySelector('.equip-slot-2');
+const equipWeapon2 = document.querySelector('.equip-weapon-2');
+
+const equipSlot3 = document.querySelector('.equip-slot-3');
+const equipWeapon3 = document.querySelector('.equip-weapon-3');
+
+
+
+
+
+// 1. Drag - box เเรก
+function dragWeapon(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    console.log(event.target);
+}
+weapons.forEach((weapon) => {
+    weapon.addEventListener('dragstart', dragWeapon);
+});
+
+// 2. Drop box เเรก
+// >> https://codepen.io/bradtraversy/pen/odmVgN
+// ฟังชั่นนี้จะทำงานเมื่อมีการลากไปจัดจุด box
+function dragOverWeapon(event) {
+    event.preventDefault();
+}
+function dropWeapon1(event) {
+    console.log("drop")
+    event.preventDefault();
+    if(!event.dataTransfer) return console.log('box one None input');
+    // จะเก็บ id class ที่เราวางลง
+    const id = event.dataTransfer.getData('text/plain');
+    const image = document.querySelector('#' + id);
+    equipWeapon1.src = image.src;
 }
 
-// Drag Functions
 
-function dragStart() {
-  this.className += 'hold';
-  setTimeout(() => (this.className = 'invisible'), 0);
+function dropWeapon2(event) {
+ 
+    event.preventDefault();
+    if(!event.dataTransfer) return console.log('box one None input');
+    // จะเก็บ id class ที่เราวางลง
+    const id = event.dataTransfer.getData('text/plain');
+    const image = document.querySelector('#' + id);
+    equipWeapon2.src = image.src
 }
 
-function dragEnd() {
-  this.className = 'fill';
+function dropWeapon3(event) {
+    event.preventDefault();
+    if(!event.dataTransfer) return console.log('box one None input');
+    // จะเก็บ id class ที่เราวางลง
+    const id = event.dataTransfer.getData('text/plain');
+    const image = document.querySelector('#' + id);
+    equipWeapon3.src = image.src;
 }
 
 
-function dragOver(e) {
-  e.preventDefault();
-}
+equipSlot1.addEventListener('dragover', dragOverWeapon);
+equipSlot1.addEventListener('drop', dropWeapon1);
 
-function dragEnter(e) {
-  e.preventDefault();
-  this.className += ' hovered';
-}
 
-function dragLeave() {
-  this.className += 'empty';
-}
+equipSlot2.addEventListener('dragover', dragOverWeapon);
+equipSlot2.addEventListener('drop', dropWeapon2);
 
-function dragDrop() {
-  this.className += 'empty';
-  this.append(fill);
+equipSlot3.addEventListener('dragover', dragOverWeapon);
+equipSlot3.addEventListener('drop', dropWeapon3);
 
-}
 
